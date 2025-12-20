c10bcd9d0bcb667ae37f225dbb8dbe9851abbad1
Aanish103 <ama103@student.le.ac.uk>
2025-12-20 01:44:21 +0000

    #40 Update Visits

--- modules/visits.py
+++ modules/visits.py
@@ -5,7 +5,7 @@ def add_visit():
     application_id = input("Application ID: ").strip()
 
     if not ds.get_by_id(ds.applications, application_id):
-        print("application_id not found. Create application first. - visits.py:8")
+        print("application_id not found. Create application first.")
         return
 
     visitor_name = input("Visitor Name: ").strip()
@@ -15,3 +15,35 @@ def add_visit():
 
     vid = ds.add_visit(application_id, visitor_name, visit_date, outcome, notes)
     print(f"Visit recorded successfully. visit_id: {vid} ")
+
+
+def update_visit():
+    vid = input("Enter Visit ID: ").strip()
+    v = ds.get_by_id(ds.visits, vid)
+
+    if not v:
+        print("Visit ID not found.")
+        return
+
+    data = {}
+
+    visitor = input(f"Visitor Name [{v['visitor_name']}]: ").strip()
+    if visitor:
+        data["visitor_name"] = visitor
+
+    visit_date = input(f"Visit Date [{v['visit_date']}]: ").strip()
+    if visit_date:
+        data["visit_date"] = visit_date
+
+    outcome = input(f"Outcome [{v['outcome']}]: ").strip()
+    if outcome:
+        data["outcome"] = outcome
+
+    notes = input(f"Notes [{v['notes']}]: ").strip()
+    if notes:
+        data["notes"] = notes
+
+    if ds.update_visit(vid, data):
+        print("Visit updated successfully.")
+    else:
+        print("Update failed.")
