8de1318ce01ba5a041ad64c95ebf6f1235f564e8
Aanish103 <ama103@student.le.ac.uk>
2025-12-20 23:45:46 +0000

    #44 Withdraw Application

--- modules/decisions.py
+++ modules/decisions.py
@@ -36,3 +36,21 @@ def reject_application():
     )
 
     print("Application rejected successfully")
+
+
+def withdraw_application():
+    aid = input("Application ID: ")
+    comment = input("Comment: ")
+    d = input("Decision date (DD/MM/YYYY) [blank for today]: ")
+
+    decision_date = d if d else date.today().strftime("%d/%m/%Y")
+
+    ds.add_decision(
+        app_id=aid,
+        d_type="Withdrawn",
+        by="System",
+        comment=comment,
+        date=decision_date
+    )
+
+    print("Application withdrawn successfully")
