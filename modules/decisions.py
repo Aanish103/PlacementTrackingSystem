54d85e7aa68f1e95d9d459b8a5bfd0952dee8918
Aanish103 <ama103@student.le.ac.uk>
2025-12-20 23:39:37 +0000

    #43 Reject Application

--- modules/decisions.py
+++ modules/decisions.py
@@ -18,3 +18,21 @@ def approve_application():
     )
 
     print("Application approved successfully")
+
+
+def reject_application():
+    aid = input("Application ID: ")
+    comment = input("Comment: ")
+    d = input("Decision date (DD/MM/YYYY) [blank for today]: ")
+
+    decision_date = d if d else date.today().strftime("%d/%m/%Y")
+
+    ds.add_decision(
+        app_id=aid,
+        d_type="Rejected",
+        by="System",
+        comment=comment,
+        date=decision_date
+    )
+
+    print("Application rejected successfully")
